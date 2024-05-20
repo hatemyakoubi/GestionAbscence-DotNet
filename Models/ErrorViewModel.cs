06dbb33b19777168c188efe9c4158c09c682de3d
Hatem Yakoubi <hatemyakoubi15@gmail.com>
2024-05-20 22:13:30 +0200

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace gestionabscence.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace gestionabscence.Models
+{
+    public class User
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        public string FullName { get; set; }
+
+        [Required]
+        public string IdCard { get; set; }
+
+        [Required]
+        public DateTime BirthDate { get; set; }
+
+        public string Address { get; set; }
+
+        public string Phone { get; set; }
+
+        [Required]
+        public Gender Gender { get; set; }
+
+        [Required]
+        public string Role { get; set; } = "Student";
+
+        // Navigation properties
+        public virtual ICollection<Student> Students { get; set; }
+        public virtual ICollection<Professor> Professors { get; set; }
+        public virtual ICollection<Admin> Admins { get; set; }
+    }
+}
